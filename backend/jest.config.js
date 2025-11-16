0d349d4b0cffdcd8b6090c2a7204c8c03f067bd1
My Health Chain <dev@myhealthchain.ke>
2025-11-16 04:54:16 +0300

    Configure backend Jest for TypeScript

---
+++ backend/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
+};
